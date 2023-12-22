7eeed576a75becf2e09b4bb723edb0bf9e7f20c9
Ngoc Hien <ngochiensunflower@gmail.com>
2023-12-22 20:04:05 +0700

    test01

--- src/components/Tinhtuoi.js
+++ src/components/Tinhtuoi.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 
 export default function Tinhtuoi() {
-    const [name, setName] = useState("Nguyễn Minh Nhân");
-    const [birthday, setBirthday] = useState("1990");
-    var CurrentYear = new Date().getFullYear();
+    const [name, setName] = useState("");
+    const [birthday, setBirthday] = useState("");
+    const [age, setAge] = useState("");
+    const [text, setText] = useState("");
+
     function handle_name(e) {
         setName(e.target.value);
     }
@@ -12,15 +14,36 @@ export default function Tinhtuoi() {
     }
     return (
         <div>
-            <labe>Nhập tên: </labe>
-            <input type="text" value={name} onChange={handle_name} />
+            {/* <input type="text" value={name} onChange={handle_name} placeholder="Nhập tên" />
+             */}
+            <input
+                type="text"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+                placeholder="Nhập tên"
+                onKeyDown={(e) => {
+                    if (e.key == "Enter") {
+                        setName(text);
+                    }
+                }}
+            />
             <br />
-            <label>Nhập năm sinh: </label>
-            <input type="number" value={birthday} onChange={handle_birthday} />
+
+            <input
+                type="number"
+                value={birthday}
+                onChange={handle_birthday}
+                placeholder="Nhập năm sinh"
+                onKeyDown={(e) => {
+                    if (e.key == "Enter") {
+                        setAge(new Date().getFullYear() - birthday);
+                    }
+                }}
+            />
             <br />
             <p>Hãy xác nhận thông tin</p>
-            <p>{name}</p>
-            <p>Tuoi: {CurrentYear - birthday}</p>
+            <p>{name && "Name: " + name}</p>
+            <p>{age && "Tuoi: " + age}</p>
         </div>
     );
 }
