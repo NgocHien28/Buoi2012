26ad62ad741cc3ae10b22268015db6c509663cb0
NgocHien <ngochiensunflower@gmail.com>
2023-12-27 21:04:37 +0700

    mockApi & useEffect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import "./App.css";
 import Account from "./components/Account";
+import ApiStudent from "./components/ApiStudent";
 import ES6sample from "./components/ES6sample";
+import Effect from "./components/Effect";
+import Login from "./components/Login";
 import Object from "./components/Object";
 // import Ex2 from "./components/Ex2";
 // import Ex1 from "./components/Ex1";
@@ -12,7 +15,10 @@ function App() {
             {/* <Ex2 /> */}
             {/* <ES6sample /> */}
             {/* <Object /> */}
-            <Account />
+            {/* <Account /> */}
+            {/* <Login /> */}
+            {/* <Effect /> */}
+            <ApiStudent />
         </div>
     );
 }
